17c5920bef4ce80e2ce7c67edd1ec8ac841f60ab
Jorge Matos <jisapjisap@gmail.com>
2023-11-02 23:29:54 +0100

    nodemon - Variables de entorno

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,6 +1,7 @@
 
 const { emailTemplate } = require('./js-foundation/01-template.js');
 
-console.log(emailTemplate);
+require ('./js-foundation/02-destructuring.js');
+
 
 //console.log(emailTemplate);
---
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -0,0 +1,7 @@
+
+
+//console.log(process.env);
+
+const { USERDOMAIN, USERNAME, npm_lifecycle_script}  = process.env;
+
+//console.table({USERDOMAIN, USERNAME, npm_lifecycle_script})
