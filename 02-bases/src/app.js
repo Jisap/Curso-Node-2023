7f560a9192d871ef84862bd2e7d9034ef543bdae
Jorge Matos <jisapjisap@gmail.com>
2023-11-05 16:37:24 +0100

    callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,7 +1,17 @@
 
-const { emailTemplate } = require('./js-foundation/01-template.js');
-
-require ('./js-foundation/02-destructuring.js');
-
+//const { emailTemplate } = require('./js-foundation/01-template.js');
+//require ('./js-foundation/02-destructuring.js');
+const { getUserById } = require('./js-foundation/03-callbacks');
 
 //console.log(emailTemplate);
+
+const id = 1
+
+getUserById(1, function( error, user){
+    
+    if(error){
+        throw new Error( error );
+    }
+
+    console.log(user);
+});
