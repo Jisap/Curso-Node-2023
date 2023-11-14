3e110ba74f037b3eb1d3467b8f87b9c9f5448393
Jorge Matos <jisapjisap@gmail.com>
2023-11-14 18:47:05 +0100

    test y migración

--- 02-bases/dist/app.js
+++ 02-bases/dist/app.js
@@ -1,42 +1,29 @@
-
+"use strict";
 //const { emailTemplate } = require('./js-foundation/01-template.js');
 //require ('./js-foundation/02-destructuring.js');
 //const { getUserById } = require('./js-foundation/03-callbacks');
 //const { getUserById } = require('./js-foundation/04-arrow');
 // const { buildMakePerson } = require('./js-foundation/05-factory');
 // const { getUUID, getAge } = require('./js-foundation/plugins');
-const getPokemonById = require('./js-foundation/06-promesas');
-
+// const getPokemonById = require('./js-foundation/06-promesas');
 //console.log(emailTemplate);
-
 // const id = 1
-
 // getUserById(1, ( error, user) => {
-    
 //     if(error){
 //         throw new Error( error );
 //     }
-
 //     console.log(user);
 // });
-
 // Factory functions
 // const obj = { name: 'john', birdthdate: '1985-10-21' }
-
 // const makePerson = buildMakePerson({ getUUID, getAge })
-
 // const john = makePerson( obj );
-
 // console.log({ john });
-
 // Promesas
 // getPokemonById(1)
 //     .then((pokemon) => console.log({ pokemon }))
-
 // logger
-const { buildLogger } = require('./js-foundation/plugins');
-
-const logger = buildLogger('app.js');
-
-logger.log('Hola mundo');
-logger.error("Test de error");
+// const { buildLogger } = require('./js-foundation/plugins');
+// const logger = buildLogger('app.js');
+// logger.log('Hola mundo');
+// logger.error("Test de error");
