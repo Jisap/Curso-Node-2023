3438558943a320df19cae9c3ff36f86dd32967da
Jorge Matos <jisapjisap@gmail.com>
2023-11-16 18:02:04 +0100

    Pruebas en 01-template

---
+++ 02-bases/tests/js-foundation/01-template.test.ts
@@ -0,0 +1,20 @@
+import { emailTemplate } from '../../src/js-foundation/01-template';
+
+
+describe('js-foundation/01-template', () => {
+  
+  test('emailTempate should contain a greeting', () => {
+    
+    expect( emailTemplate ).toContain('Hi, ');
+
+  });
+
+  test('emailTemplate should contain {{name}} and {{orderId}}', () => { 
+  
+      expect(emailTemplate).toMatch(/{{name}}/);
+      expect(emailTemplate).toMatch(/{{orderId}}/);
+  })
+
+
+
+});
