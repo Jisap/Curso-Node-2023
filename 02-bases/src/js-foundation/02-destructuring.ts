0245ca7b0b29fceacc3dff682998441c20ee081b
Jorge Matos <jisapjisap@gmail.com>
2023-11-17 18:38:25 +0100

    test 02-destructuring.ts

--- 02-bases/src/js-foundation/02-destructuring.ts
+++ 02-bases/src/js-foundation/02-destructuring.ts
@@ -5,3 +5,7 @@
 const { USERDOMAIN, USERNAME, npm_lifecycle_script}  = process.env;
 
 //console.table({USERDOMAIN, USERNAME, npm_lifecycle_script})
+
+export const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
+
+const [, , ,batman] = characters;
