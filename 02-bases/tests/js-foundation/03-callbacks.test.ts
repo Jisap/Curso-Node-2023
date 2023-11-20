df09cb6686d7001c624b0d801c0c45fd5ed659ac
Jorge Matos <jisapjisap@gmail.com>
2023-11-20 15:59:49 +0100

    callbacks test

---
+++ 02-bases/tests/js-foundation/03-callbacks.test.ts
@@ -0,0 +1,15 @@
+import { getUserById } from '../../src/js-foundation/03-callbacks';
+
+
+
+describe('js-foundation/03-callbacks.ts', () => { 
+
+  test('getUserById should return an error if user does not exist', () => {
+
+    const id = 10;
+    getUserById(id, (err, user) => {  
+      expect( err ).toBe(`User not found with id ${id}`);
+      expect( user ).toBeUndefined();
+    })
+  })
+})
