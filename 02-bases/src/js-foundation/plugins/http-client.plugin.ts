2f72768d86f82eea15f40b1abba59ae6d24736af
Jorge Matos <jisapjisap@gmail.com>
2023-11-25 17:49:12 +0100

    httpClientPlugin test

--- 02-bases/src/js-foundation/plugins/http-client.plugin.ts
+++ 02-bases/src/js-foundation/plugins/http-client.plugin.ts
@@ -12,5 +12,15 @@ export const httpClientPlugin = {
     //axios
     const { data } = await axios.get(url);
     return data;
-  }
-}
+  },
+
+  post: async(url: string, body: any) => {
+    throw new Error('Not implemented');
+  },
+  put:async (url: string, body: any) => {
+    throw new Error('Not implemented');
+  },
+  delete:async (url: string) => {
+    throw new Error('Not implemented');
+  },
+};
