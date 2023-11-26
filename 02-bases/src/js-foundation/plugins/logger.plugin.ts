c9ad722b8bc078f0437278303f049100658e594c
Jorge Matos <jisapjisap@gmail.com>
2023-11-26 11:38:59 +0100

    logger test

--- 02-bases/src/js-foundation/plugins/logger.plugin.ts
+++ 02-bases/src/js-foundation/plugins/logger.plugin.ts
@@ -2,7 +2,7 @@
 import  winston from 'winston'
 const { combine, timestamp, json } = winston.format;
 
-const logger = winston.createLogger({               // Objeto logger
+export const logger = winston.createLogger({        // Objeto logger
     level: 'info',                                  // nivel de importancia para los mensajes tipo "info"
     format: combine(                                // Con un formato que combine
         timestamp(),                                // los tiempos en que se ejecuto
@@ -10,10 +10,7 @@ const logger = winston.createLogger({               // Objeto logger
     ),                  
     //defaultMeta: { service: 'user-service' },     // metadatos en todos los registros
     transports: [                                   // Los transports son los destinos de los registros     
-        //
-        // - Write all logs with importance level of `error` or less to `error.log`
-        // - Write all logs with importance level of `info` or less to `combined.log`
-        //
+       
         new winston.transports.File({ filename: 'error.log', level: 'error' }), // Transport de nivel error en archivo llamado error.log
         new winston.transports.File({ filename: 'combined.log' }),              // Transport de nivel info en archivo llamando combined.log
     ],
@@ -26,7 +23,7 @@ logger.add(new winston.transports.Console({ // console.log tradicional
 // Patron adaptador de winston
 export const buildLogger = ( service:string ) => { // buildLogger llama al servicio (app) al que pertenece el registro
     return {
-        log: ( message: string ) => {                        // retornará un método log que recibirá el mensaje que queremos registrar en logs
+        log: ( message: string ) => {                // retornará un método log que recibirá el mensaje que queremos registrar en logs
             logger.log( 'info', {message, service} ) // Este mensaje tendrá el lvl, el formato y servicio (app) que se escribirá en los logs
         },
         error: (message:string) => {
