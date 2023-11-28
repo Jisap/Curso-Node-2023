a620e8f54232f29c60e2507faad01c89de1217d2
Jorge Matos <jisapjisap@gmail.com>
2023-11-28 18:07:15 +0100

    04-multiplicationApp

--- 04-multiplicationApp/src/app.ts
+++ 04-multiplicationApp/src/app.ts
@@ -1,4 +1,24 @@
+import  fs, { mkdirSync } from 'fs';
 
-const message:string = 'hola mundo'
 
-console.log(message);
+
+let outputMessage = '';
+const base = 5;
+const headerMessage = `
+================================
+        Tabla del ${base}
+================================\n
+`;
+
+for ( let i = 1; i <= 10; i++){
+  outputMessage += `${base} x ${i} = ${base * i}\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+const outputPath = `outputs`;
+fs.mkdirSync(outputPath, {recursive: true});
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+
+console.log('File Created')
