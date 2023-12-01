bc24c01d153713da63a1aa127043be1936042076
Jorge Matos <jisapjisap@gmail.com>
2023-12-01 17:51:26 +0100

    Función asíncrona autoinvocada

--- 04-multiplicationApp/src/app.ts
+++ 04-multiplicationApp/src/app.ts
@@ -2,4 +2,13 @@ import { yarg } from "./config/plugins/yargs.plugin";
 
 
 
-console.log(yarg)
+// console.log(yarg)
+
+(async () => {  // Función asíncrona autoinvocada
+  await main();
+  console.log('Fin de programa')
+})();
+
+async function main(){
+  console.log('Main ejecutado')
+}
