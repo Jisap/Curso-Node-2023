7772f2b41bccde3c664a74505dd0ab8f0803dbda
Jorge Matos <jisapjisap@gmail.com>
2023-12-19 19:09:06 +0100

    pruebas en App.ts

--- 04-multiplicationApp/src/app.test.ts
+++ 04-multiplicationApp/src/app.test.ts
@@ -1,10 +1,24 @@
+import { ServerApp } from './presentation/server-app';
 
 
 
-describe('App', () => { 
+describe('Test App.ts', () => { 
 
-  test('should be true', () => { 
-    expect(true).toBe(true);
-  })
+  test('should call Server.run with values', async() => { 
+  
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = ['node', 'app.ts', '-b', '10', '-l', '-s', '-n', 'test-file', '-d', 'test-destination']
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 10,
+      showTable: true,
+      fileName: 'test-file',
+      destination: 'test-destination'
+    })
+  });
 
 })
