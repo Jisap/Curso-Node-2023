dc72d75c03af274fa48316754855b658d5bce598
Jorge Matos <jisapjisap@gmail.com>
2023-12-21 18:19:04 +0100

    CronService

--- 05-nocs/src/presentation/server.ts
+++ 05-nocs/src/presentation/server.ts
@@ -1,18 +1,32 @@
-import { CronJob} from 'cron'
+import { CronService } from "./cron/cron-service";
+
 
 
 export class Server {
   public static start() {
+
     console.log('Server started...');
 
-    const job = new CronJob(
-      '/2 * * * * *',
-       () => {
-        const date = new Date()
-        console.log('2 second', date);
-      }, 
+    CronService.createJob(
+      '*/5 * * * * *',
+      () => {
+        const date = new Date();
+        console.log('5 secons', date)
+      }  
     );
-
-    job.start();
+    CronService.createJob(
+      '*/2 * * * * *',
+      () => {
+        const date = new Date();
+        console.log('2 secons', date)
+      }
+    );
+    CronService.createJob(
+      '*/3 * * * * *',
+      () => {
+        const date = new Date();
+        console.log('3 secons', date)
+      }
+    );
   }
 }
