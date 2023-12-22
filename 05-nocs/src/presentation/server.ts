7c27fbf6c0b20eb8ce77e7387200946507af1ea6
Jorge Matos <jisapjisap@gmail.com>
2023-12-22 18:27:56 +0100

    checkservice use-case

--- 05-nocs/src/presentation/server.ts
+++ 05-nocs/src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 
@@ -10,23 +11,9 @@ export class Server {
     CronService.createJob(
       '*/5 * * * * *',
       () => {
-        const date = new Date();
-        console.log('5 secons', date)
+        new CheckService().execute('https://google.com')
       }  
     );
-    CronService.createJob(
-      '*/2 * * * * *',
-      () => {
-        const date = new Date();
-        console.log('2 secons', date)
-      }
-    );
-    CronService.createJob(
-      '*/3 * * * * *',
-      () => {
-        const date = new Date();
-        console.log('3 secons', date)
-      }
-    );
+   
   }
 }
