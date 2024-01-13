68da83a250160165fa20fd5bfc5e47d446f0914b
Jorge Matos <jisapjisap@gmail.com>
2024-01-13 18:32:19 +0100

    Schema & models

---
+++ 05-nocs/src/data/mongoose/models/log.model.ts
@@ -0,0 +1,31 @@
+// Entidad
+
+  // public level: LogSeverityLevel;
+  // public message: string;
+  // public createdAt: Date;
+  // public origin: string; 
+
+import mongoose from "mongoose";
+
+const logSchema = new mongoose.Schema({
+
+  message: {
+    type: String,
+    required: true
+  },
+  origin: {
+    type: String,
+  },
+  level: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'low'
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+
+});
+
+export const LogModel = mongoose.model('Log', logSchema)
