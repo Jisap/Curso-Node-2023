9f066ce13d042a6703161e0007d539b5d2c2ad1a
Jorge Matos <jisapjisap@gmail.com>
2024-01-13 22:28:05 +0100

    crear y leer de mongo

--- 05-nocs/src/app.ts
+++ 05-nocs/src/app.ts
@@ -1,7 +1,7 @@
 
 
 import { envs } from "./config/plugins/envs";
-import { MongoDatabase } from "./data/mongoose";
+import { LogModel, MongoDatabase } from "./data/mongoose";
 
 import { Server } from "./presentation/server";
 
@@ -16,8 +16,21 @@ async function main() {
 
   await MongoDatabase.connect({
     mongoUrl: envs.MONGO_URL,
-    dbName: envs.MONGO_DB_NAME
-  })
+    dbName: envs.MONGO_DB_NAME,
+
+  });
+
+  //Crear una colección = tables, document = registro
+  const newLog = await LogModel.create({
+    message: 'Test message desde Mongo',
+    origin: ' App.ts',
+    level: 'low'
+  });
+
+  //await newLog.save();
+
+  console.log(newLog)
+
 
   //Server.start()
 }
