a0be7318c5fbb9b09736e69e51f71cf03fc6e265
Jorge Matos <jisapjisap@gmail.com>
2024-01-29 16:02:38 +0100

    Pruebas en file-system.datasource 1

---
+++ 05-nocs/src/infraestructure/datasources/file-system.datasource.test.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { FileSystemDataSource } from './file-system.datasource';
+
+
+describe('file-system.datasource.ts', () => {
+
+  const logPath = path.join(__dirname, '../../../logs')
+
+  beforeEach(() => {
+    fs.rmSync(logPath, {recursive: true, force: true})
+  })
+
+  test('should create log files if they do not exists', () => {
+    
+    new FileSystemDataSource();
+    const files = fs.readdirSync(logPath);
+    expect(files).toEqual(['logs-all.log', 'logs-high.log', 'logs-medium.log'])
+  });
+  
+})
