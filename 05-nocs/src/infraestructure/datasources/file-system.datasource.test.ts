e4818ec096743c4b1ec1a74529182e5b6716e736
Jorge Matos <jisapjisap@gmail.com>
2024-01-29 16:32:18 +0100

    file-system.datasource2

--- 05-nocs/src/infraestructure/datasources/file-system.datasource.test.ts
+++ 05-nocs/src/infraestructure/datasources/file-system.datasource.test.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { FileSystemDataSource } from './file-system.datasource';
+import { LogDataSource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 
 describe('file-system.datasource.ts', () => {
@@ -17,5 +19,86 @@ describe('file-system.datasource.ts', () => {
     const files = fs.readdirSync(logPath);
     expect(files).toEqual(['logs-all.log', 'logs-high.log', 'logs-medium.log'])
   });
+
+  test('should save a log in logs-all.log', () => {
+    
+    const LogDataSource = new FileSystemDataSource();
+    const log = new LogEntity({
+      message: 'test',
+      level: LogSeverityLevel.low,
+      origin: 'file-system.datasource.test.ts'
+    });
+    LogDataSource.saveLog(log);
+    const allLogs = fs.readFileSync(`${logPath}/logs-all.log`, 'utf-8');
+    
+    expect(allLogs).toContain(JSON.stringify(log))
+  });
+
+  test('should save a log in logs-all.log and logs-medium.log', () => {
+
+    const LogDataSource = new FileSystemDataSource();
+    const log = new LogEntity({
+      message: 'test',
+      level: LogSeverityLevel.medium,
+      origin: 'file-system.datasource.test.ts'
+    });
+    LogDataSource.saveLog(log);
+    const allLogs = fs.readFileSync(`${logPath}/logs-all.log`, 'utf-8');
+    const mediumLogs = fs.readFileSync(`${logPath}/logs-medium.log`, 'utf-8');
+
+    expect(allLogs).toContain(JSON.stringify(log));
+    expect(mediumLogs).toContain(JSON.stringify(log))
+  });
+
+  test('should save a log in logs-all.log and logs-high.log', () => {
+
+    const LogDataSource = new FileSystemDataSource();
+    const log = new LogEntity({
+      message: 'test',
+      level: LogSeverityLevel.high,
+      origin: 'file-system.datasource.test.ts'
+    });
+    LogDataSource.saveLog(log);
+    const allLogs = fs.readFileSync(`${logPath}/logs-all.log`, 'utf-8');
+    const highLogs = fs.readFileSync(`${logPath}/logs-high.log`, 'utf-8');
+
+    expect(allLogs).toContain(JSON.stringify(log));
+    expect(highLogs).toContain(JSON.stringify(log))
+  });
+
+  test('should return all logs', async() => {
+    
+    const LogDataSource= new FileSystemDataSource();
+    const logLow = new LogEntity({
+      message: 'log-low',
+      level: LogSeverityLevel.low,
+      origin: 'low'
+    });
+
+    const logMedium = new LogEntity({
+      message: 'log-medium',
+      level: LogSeverityLevel.medium,
+      origin: 'medium'
+    });
+
+    const logHigh = new LogEntity({
+      message: 'log-high',
+      level: LogSeverityLevel.high,
+      origin: 'hig'
+    });
+
+    await LogDataSource.saveLog(logLow);
+    await LogDataSource.saveLog(logMedium);
+    await LogDataSource.saveLog(logHigh);
+
+    const logsLow = await LogDataSource.getLogs(LogSeverityLevel.low);
+    const logsMedium = await LogDataSource.getLogs(LogSeverityLevel.medium);
+    const logsHigh = await LogDataSource.getLogs(LogSeverityLevel.high);
+
+    expect(logsLow).toEqual(expect.arrayContaining([logLow, logMedium, logHigh]));
+    expect(logsMedium).toEqual(expect.arrayContaining([ logMedium]));
+    expect(logsHigh).toEqual(expect.arrayContaining([logHigh]));
+  });
+  
   
 })
