89ffed0c4d4190327a011255fd403cab0f15bc7a
Jorge Matos <jisapjisap@gmail.com>
2024-02-10 15:22:13 +0100

    Servir SPA con router

--- 07-restweb/src/presentation/server.ts
+++ 07-restweb/src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path';
 
 
 
@@ -12,6 +13,11 @@ export class Server {
 
     // public folder
     this.app.use( express.static('public'));
+
+    this.app.get('*', (req, res) => {
+      const indexPath = path.join( __dirname + '../../../public/index.html')
+      res.sendFile(indexPath);
+    })
     
     this.app.listen(3000, () => {
       console.log(`Server running on port ${3000}`);
