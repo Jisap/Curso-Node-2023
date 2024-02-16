01f24db202f5cab3d2204fd7811d454c4f5fd154
Jorge Matos <jisapjisap@gmail.com>
2024-02-16 17:07:48 +0100

    update Todo

--- 07-restweb/src/presentation/todos/controller.ts
+++ 07-restweb/src/presentation/todos/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
 const todos = [
-  { id: 1, text: 'Buy milk', createdAt: new Date() },
-  { id: 2, text: 'Buy bread', createdAt: null },
-  { id: 3, text: 'Buy butter', createdAt: new Date() },
+  { id: 1, text: 'Buy milk', completedAt: new Date() },
+  { id: 2, text: 'Buy bread', completedAt: null },
+  { id: 3, text: 'Buy butter', completedAt: new Date() },
 ]
 
 export class TodosController {
@@ -26,8 +26,33 @@ export class TodosController {
   }
 
   public createTodo = (req: Request, res: Response) => {
-    const body = req.body;
+    const { text } = req.body;
+    if(!text) return res.status(400).json({error: 'text property is required'})
     
-    res.json(body)
+    const newTodo = {
+      id: todos.length + 1,
+      text: text,
+      completedAt: null
+    }
+
+    todos.push( newTodo );
+
+    res.json( newTodo )
+  }
+
+  public updateTodo = (req: Request, res: Response) => {
+
+    const id = +req.params.id
+    if (isNaN(id)) return res.status(400).json({ error: 'Id argument is not a number' });
+
+    const todo = todos.find(todo => todo.id === id);
+    if(!todo) return res.status(404).json({ error: `Todo with id ${ id } not found`});
+
+    const {text, completedAt} = req.body;
+    
+    todo.text = text || todo.text;
+    (completedAt === null )
+
+    res.json(todo)
   }
 }
