44c499f6e6607fec287340a43569e10786cc5320
Jorge Matos <jisapjisap@gmail.com>
2024-02-23 17:13:10 +0100

    domain - datasources - repositories

---
+++ 07-restweb/src/domain/repositories/todo.repository.ts
@@ -0,0 +1,15 @@
+import { TodoEntity } from "../entities/todo.entity";
+import { CreateTodoDto } from '../dtos/todos/create-todo.dto';
+import { UpdateTodoDto } from '../dtos/todos/update-todo.dto';
+
+
+
+export abstract class TodoRepository {
+
+  abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;   // Crea un todoEntity desde un CreateTodoDto
+  abstract getAll(): Promise<TodoEntity[]>                              // Obtiene un [] de TodoEntity
+  abstract findById(id: number): Promise<TodoEntity>                    // Busca por id un TodoEntity
+  abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>// Actualiza por id (updateTodoDto) un TodoEntity   
+  abstract updateById(id: number): Promise<TodoEntity>                  // Borra por id un TodoEntity
+
+}
