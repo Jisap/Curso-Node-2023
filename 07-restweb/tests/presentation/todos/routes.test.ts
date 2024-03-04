a5258192afd42b6bb74be88bba0aa7376fc35a08
Jorge Matos <jisapjisap@gmail.com>
2024-03-04 15:57:55 +0100

    GET api/todos/:id

--- 07-restweb/tests/presentation/todos/routes.test.ts
+++ 07-restweb/tests/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres/index';
 
 
 
@@ -13,14 +14,47 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  beforeEach(async() => {
+    await prisma.todo.deleteMany();
+  })
+
+  const todo1 = { text: "Hola mundo 1" };
+  const todo2 = { text: "Hola mundo 2" };
+
   test('should return the TODOs api/todos', async() => {
-    
-    const response = await request(testServer.app)
+     
+    await prisma.todo.createMany({
+      data: [ todo1, todo2 ]
+    })
+
+    const { body } = await request(testServer.app)
       .get('/api/todos')
       .expect(200)
 
-    console.log(response.body)
+    expect( body ).toBeInstanceOf( Array )
+    expect( body.length ).toBe(2)
+    expect( body[0].text).toBe( todo1.text )
+    expect( body[1].text).toBe( todo2.text )
+    expect( body[0].completedAt ).toBeNull()
+
+    console.log(body)
 
   });
+
+  test('Should return a TODO api/todos/:id', async() => {
+    
+    const newTodo = await prisma.todo.create({ data: todo1 });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${newTodo.id}`)
+      .expect(200);
+
+    expect(body).toEqual({
+      id: newTodo.id,
+      text: newTodo.text,
+      completedAt: newTodo.completedAt
+    });
+  });
+  
   
 })
