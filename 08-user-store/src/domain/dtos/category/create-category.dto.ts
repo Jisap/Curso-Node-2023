730f1a2cedcd7891f3d85b74a9ec09098a7dca3a
Jorge Matos <jisapjisap@gmail.com>
2024-03-17 12:49:30 +0100

    createCategoryDto

---
+++ 08-user-store/src/domain/dtos/category/create-category.dto.ts
@@ -0,0 +1,26 @@
+
+
+
+export class CreateCategoryDto {
+
+  private constructor(
+    public readonly name: string,
+    public readonly available: boolean,  
+  ){}
+                            // clave   valor     error     instancia dto
+  static create( object: { [key:string]:any}):[string?, CreateCategoryDto?] {
+    
+    const { name, available = false } = object;
+    
+    let availableBoolean = available
+    
+    if(!name) return ['Missing name'];
+    if(typeof available !== 'boolean'){           // Si available no es un boolean, osea un string
+      availableBoolean = (available === 'true')   // comprobamos si ese string = 'true', y si lo es availableBoolean = true
+    }                                             // de lo contrario será false
+            // error        Dto
+    return [undefined, new CreateCategoryDto(name, availableBoolean)]
+
+  }
+
+}
