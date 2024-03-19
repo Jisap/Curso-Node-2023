a995fd948472724f3c5fdd0728c16ddc93f59ef7
Jorge Matos <jisapjisap@gmail.com>
2024-03-19 13:24:40 +0100

    auth-middleware

--- 08-user-store/src/domain/dtos/category/create-category.dto.ts
+++ 08-user-store/src/domain/dtos/category/create-category.dto.ts
@@ -15,7 +15,7 @@ export class CreateCategoryDto {
     let availableBoolean = available
     
     if(!name) return ['Missing name'];
-    if(typeof available !== 'boolean'){           // Si available no es un boolean, osea un string
+    if(typeof available !== 'boolean'){           // Si available no es un boolean, osea es un string
       availableBoolean = (available === 'true')   // comprobamos si ese string = 'true', y si lo es availableBoolean = true
     }                                             // de lo contrario será false
             // error        Dto
