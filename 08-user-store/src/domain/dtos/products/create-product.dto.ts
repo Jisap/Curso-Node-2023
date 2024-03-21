29b7108717c2163aeab7d73949403a9761603d6a
Jorge Matos <jisapjisap@gmail.com>
2024-03-21 18:01:09 +0100

    CreateProductDto

---
+++ 08-user-store/src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,43 @@
+
+
+
+
+export class CreateProductDto {
+
+  private constructor(
+
+    public readonly name: string,
+    public readonly available: boolean,
+    public readonly price: number,
+    public readonly description: string,
+    public readonly user: string, // id
+    public readonly category: string, // id
+  ) { }
+
+  // clave   valor     error     instancia dto
+  static create(object: { [key: string]: any }): [string?, CreateProductDto?] {
+
+    const {
+      name,
+      available,
+      price,
+      description,
+      user,
+      category
+    } = object;
+
+    if (!name) return ['Missing name'];
+    if (!user) return ['Missing user'];
+    if (!category) return ['Missing category'];
+
+    return [undefined, new CreateProductDto(
+      name,
+      !!available,
+      price,
+      description,
+      user,
+      category,
+    )]
+
+  }
+}
