1e15a66312defc1c64ce9c54170724f76ea72365
Jorge Matos <jisapjisap@gmail.com>
2024-03-22 17:12:59 +0100

    seed

--- 08-user-store/src/data/seed/seed.ts
+++ 08-user-store/src/data/seed/seed.ts
@@ -1,5 +1,9 @@
 import { envs } from "../../config"
-import { MongoDatabase } from "../mongo/mongo-database"
+import { CategoryModel, MongoDatabase, ProductModel, UserModel } from "../mongo";
+import { seedData } from "./data";
+
+
+
 
 
 (async()=> {
@@ -11,8 +15,43 @@ import { MongoDatabase } from "../mongo/mongo-database"
   await main();
 
   await MongoDatabase.disconnect()
-})()
+})();
+
+const randomBetween0AndX = ( x:number ) => {
+  return Math.floor( Math.random() * x );
+}
 
 async function main(){
+  // Borrar todo
+  await Promise.all([
+    UserModel.deleteMany(),
+    CategoryModel.deleteMany(),
+    UserModel.deleteMany(),
+  ]);
 
+  // Crear usuarios
+  const users = await UserModel.insertMany( seedData.users );
+
+  // Crear Categorías
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map(category => {
+      return {
+        ...category,
+        user: users[0]._id
+      }
+    })  
+  );
+
+  // Crear productos
+  const  products = await ProductModel.insertMany(
+    seedData.products.map( product => {
+      return{
+        ...product,
+        user: users[randomBetween0AndX( seedData.users.length - 1 )]._id,
+        category: categories[randomBetween0AndX(seedData.categories.length - 1)]._id
+      }
+    })  
+  )
+
+  console.log('SEEDED');
 }
