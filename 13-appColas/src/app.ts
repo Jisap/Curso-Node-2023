171f64f3eb973eaf495db615d4a9c5d880343396
Jorge Matos <jisapjisap@gmail.com>
2024-04-07 18:18:24 +0200

    endpoints REST para app

--- 13-appColas/src/app.ts
+++ 13-appColas/src/app.ts
@@ -19,6 +19,7 @@ function main() {
 
   // Tenemos un servidor de express para peticiones http que implementa un servicio de websockets.
   // El servidor HTTP creado con Express puede manejar tanto las solicitudes HTTP regulares como las conexiones WebSocket.
+  // Los websockets se inician con un handshake http y esto inicia el cambio de protocolo de servidor de http a ws
 
   const httpServer = createServer( server.app );  // Servidor de peticiones http basado en el server de express
   WssService.initWss({ server: httpServer })      // Se inicia el servicio de websockets basado en el server de http
