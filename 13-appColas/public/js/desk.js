cb98863b2e58969c5b2de2a00a688de274034ad6
Jorge Matos <jisapjisap@gmail.com>
2024-04-12 18:48:06 +0200

    Detalles de pantalla de escritorio

--- 13-appColas/public/js/desk.js
+++ 13-appColas/public/js/desk.js
@@ -1,22 +1,45 @@
 
 const lblPending = document.querySelector('#lbl-pending'); 
+const deskHeader = document.querySelector('h1');
+const noMoreAlert = document.querySelector('.alert');
 
-async function loadInitialCount() {
-  const pending = await fetch('/api/ticket/pending')
+const searchParams = new URLSearchParams(window.location.search); // params que se reciben en url desde el input del index.html (escritorio x)
+if(!searchParams.has('escritorio')){                              // Sino se envía el escritorio redirección al index y error
+  window.location = 'index.html';
+  throw new Error('Escritorio es requerido');
+}
+const deskNumber = searchParams.get('escritorio');                // Si si se envía el escritorio
+deskHeader.innerText = deskNumber;                                // se modifica el h1
+
+
+function checkTicketCount(currentCount = 0) {   
+  if (currentCount === 0) {                                       // Si la cuenta de tickets = 0  
+    noMoreAlert.classList.remove('d-none');                       // quita la clase d-none que quita la alerta  
+  } else {                                                        // sino
+    noMoreAlert.classList.add('d-none');                          // la añade y vemos la alerta de los tickets que quedan.
+  }
+                           
+  lblPending.innerHTML = currentCount;                            // Actualiza los tickets pendiente en html con el arg recibido                                          
+}
+
+
+async function loadInitialCount() {                               // Muestra los ticket no asignados a un escritorio
+  const pendingTickets = await fetch('/api/ticket/pending')
     .then(resp => resp.json());
 
-  lblPending.innerHTML = pending.length || 0;
+  checkTicketCount(pendingTickets.length);                        // Actualiza pendientes en el html  
 }
 
 function connectToWebSockets() {
 
   const socket = new WebSocket('ws://localhost:3000/ws');
 
-  socket.onmessage = (event) => {   // A la escucha del  Evento on-ticket-count-changed
+  socket.onmessage = (event) => {   // A la escucha del  Evento on-ticket-count-changed (emitido al crear un ticket) y que devuelve el nº de tickets pendientes
     console.log(event.data); 
     const { type, payload } = JSON.parse(event.data);    
     if (type !== 'on-ticket-count-changed') return; // Si el type es 'on-ticket-count-changed' cambiamos el html
-    lblPending.innerHTML = payload;
+  
+    checkTicketCount(payload);      // Actualiza pendientes en el html
   };
 
   socket.onclose = (event) => {
--- 13-appColas/public/js/new-ticket.js
+++ 13-appColas/public/js/new-ticket.js
@@ -2,25 +2,25 @@
 
 
 
-const currentTicketLbl = document.querySelector('span');
-const createTicketBtn = document.querySelector('button');
+const currentTicketLbl = document.querySelector('span');  // Donde se muestran los tickets
+const createTicketBtn = document.querySelector('button'); // Botón de generar tickets
 
 async function getLastTicket() {
-  const lastTicket = await fetch('/api/ticket/last')
+  const lastTicket = await fetch('/api/ticket/last')      // Petición para obtener el último ticket
     .then(resp => resp.json())
   
-  currentTicketLbl.innerText = lastTicket;
+  currentTicketLbl.innerText = lastTicket;                // Actualización del html 
 }
 
-async function createTicket() {
+async function createTicket() {                           // Peticion para crear nuevo ticket
   const newTicket = await fetch('/api/ticket', {
     method: 'POST'  
   }).then( resp => resp.json());
 
-  currentTicketLbl.innerText = newTicket.number;
+  currentTicketLbl.innerText = newTicket.number;          // Modificación del html
 }
 
-createTicketBtn.addEventListener('click', createTicket)
+createTicketBtn.addEventListener('click', createTicket)   // Listener para el evento click -> createTicket
 
 
-getLastTicket()
+getLastTicket();                                          // Cuando se entra a new-ticket.html se obtiene el último ticket  
